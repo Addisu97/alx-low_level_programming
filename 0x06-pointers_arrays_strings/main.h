16b37327c4ee98194bd68cce678efff51366a281
Addisu97 <Addisu2008@gmail.com>
2022-03-25 07:16:36 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,15 @@
-int _putchar(char);
-char *_memset(char *s, char b, unsigned int n);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: Header file containing prototypes for all functions
+ *       used in the 0x06-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+int _putchar(char c);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -8,3 +18,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif
