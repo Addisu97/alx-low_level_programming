fce632f4c2e0b49b0109520a95269d09628ff936
Addisu97 <Addisu2008@gmail.com>
2022-04-04 07:18:09 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,43 +1,85 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* **strtow - function that splits a string into words.
-* @str : pointer
-* Return: Char
-*/
+ * copychars - copies chars to buffer
+ * @b: destination buffer
+ * @start: starting char pointer
+ * @stop: ending char pointer
+ */
+void copychars(char *b, char *start, char *stop)
+{
+	while (start <= stop)
+		*b++ = *start++;
+	*b = 0;
+}
 
+/**
+ * wordcount - counts the number of words
+ * @str: the sentence string
+ *
+ * Return: int number of words
+ */
+int wordcount(char *str)
+{
+	int words = 0, in_word = 0;
+
+	while (1)
+	{
+		if (*str == ' ' || !*str)
+		{
+			if (in_word)
+				words++;
+			in_word = 0;
+			if (!*str)
+				break;
+		}
+		else
+			in_word++;
+		str++;
+	}
+	return (words);
+}
+
+/**
+ * strtow - splits sentence into words
+ * @str: the sentence string
+ *
+ * Return: pointer to string array
+ */
 char **strtow(char *str)
 {
-	char **d;
-	int i;
-	int j = 0;
-	int con = 0;
+	int words = 0, in_word = 0;
+	char **ret, *word_start;
 
-	if (str == NULL)
+	if (!str || !*str || !wordcount(str))
 		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
+	ret = malloc(sizeof(char *) * (wordcount(str) + 1));
+	while (1)
 	{
-		if (str[i] != 32)
-			con++;
-	}
-
-	d = malloc(sizeof(char) * con);
-
-	if (d == NULL)
-		return (NULL);
-
-	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] != 32)
+		if (*str == ' ' || !*str)
 		{
-			*d[j] = str[i];
-			j++;
+			if (in_word)
+			{
+				ret[words] = malloc(sizeof(char) * (in_word + 1));
+				if (!ret[words])
+				{
+					return (NULL);
+				}
+				copychars(ret[words], word_start, str - 1);
+				words++;
+				in_word = 0;
+			}
+			if (!*str)
+				break;
 		}
 		else
 		{
+			if (!in_word++)
+				word_start = str;
 		}
+		str++;
 	}
-	return (d);
+	ret[words] = 0;
+	return (ret);
 }
