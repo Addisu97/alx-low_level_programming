a12ec7e7df0ca9245d285f9cf4db65f34cea2da4
Addisu97 <Addisu2008@gmail.com>
2022-04-05 04:56:00 -0700

    Add Files

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
-<<<<<<< HEAD
-ar -rc libmy.a *.o
-ar -t libmy.a
-ranlib libmy.a
-=======
-ar -rc liball.a *.o
-ar -t liball.a
-ranlib liball.a
->>>>>>> 4732a7781bd3e369aecc6e197a5d3be343e48fe7
+files=( *.c )
+gcc -c -Wall -Werror -Wextra -pedantic "${files[@]}"
+ar -rc libmy.a "${files[@]/%.c/.o}"
+rm "${files[@]/%.c/.o}"
