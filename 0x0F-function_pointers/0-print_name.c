c4dd900d4a5b0e917ac2099f54eecfc37d0923f7
Addisu97 <Addisu2008@gmail.com>
2022-04-14 07:08:23 -0700

    x

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-#include "fun_main.h"
+#include "function_pointers.h"
+/**
+ * * @name: name of a person.
+ * @f: print name as is and UPPERCASE.
+ * Return: Always 0.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
