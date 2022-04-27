2c693811b3546f249d565141cc2e7bba72532852
Addisu97 <Addisu2008@gmail.com>
2022-04-27 07:38:47 -0700

    Comit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,12 @@
+??? from here until ???END lines may have been inserted/deleted
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
+size_t print_listint(const listint_t *h)
+{
+	con
+???END
