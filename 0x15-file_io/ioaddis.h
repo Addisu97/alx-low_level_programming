cce2ec00d1ae7c0e1d11dd0ae6ec7898eb382cd6
Addisu97 <Addisu2008@gmail.com>
2022-05-21 02:54:28 -0700

    IO

---
+++ 0x15-file_io/ioaddis.h
@@ -0,0 +1,13 @@
+#ifndef IOADDIS_H
+#define IOADDIS_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+
+#endif
