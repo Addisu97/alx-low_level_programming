d8d93cf1b71390f8fcc635acdbc2e1bbe82f2839
Addisu97 <Addisu2008@gmail.com>
2022-06-20 12:35:27 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "list.h"
 
 /**
  * print_dlistint - Prints all elements of ll
@@ -9,13 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	unsigned int count = 0;
+	size_t i = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		i++;
 	}
-	return (count);
+	return (i);
 }
